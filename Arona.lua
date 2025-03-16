fc4492848255149053cbc9625c99be4514c3d4c0
Weave <116688306+Weave-Keys@users.noreply.github.com>
2025-03-16 16:05:10 -0600

    Update Arona.lua

--- Arona.lua
+++ Arona.lua
@@ -1,8 +1,10 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/youngstar"))();
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Weave-Keys/jmdik/refs/heads/main/Ui2"))();
 local myself = game.Players.LocalPlayer
 local character = myself.Character or myself.CharacterAdded:Wait()
 local workspace = game:GetService("Workspace")
 local consumableSpawns = workspace:WaitForChild("ConsumableSpawns")
+local bossModels = workspace.BossModels:GetChildren()
+local GiveawayM =  myself.PlayerGui.UI.ItemLauncherFrame.ItemLauncher
 local Players = game:GetService("Players")
 local delay = 0.45
 local AutoGems = false
@@ -43,7 +45,8 @@ local function isUserAdmin(userId)
     return false
 end
 
-local Window = library:CreateWindow("Arona");
+local Window = library:CreateWindow("Arona-Private");
+local Window2 = library:CreateWindow("Arona-Misc");
 
 Window:Toggle("AutoGems",function(v)
      AutoGems = v;
@@ -56,6 +59,11 @@ end);
 Window:Toggle("AntiAdmin",function(v)
     AntiAdmin = v;
 end);
+
+Window2:Toggle("Show Giveaway Menu",function(v)
+    GiveawayM.Visible = v;
+end);
+
 -- AutoGems Function
 spawn(function()
     while wait(1) do
@@ -82,20 +90,34 @@ end);
 --AutoBoss Function
 spawn(function()
     while wait(1) do
-        
+        if AutoBoss then
+            for _, boss in ipairs(bossModels) do
+                if boss.Name ~= "CupidsFire" and boss:FindFirstChild("Head") and boss:FindFirstChild("HitsLeft") and AutoBoss == true then
+                    -- Loop teleport to the boss until HitsLeft is 0
+                    local hitsLeft = boss.HitsLeft
+                    while hitsLeft.Value > 0 and AutoBoss == true do
+                        -- Teleport to the boss's position
+                        character:MoveTo(boss.LeftLowerLeg.Position)
+                        wait(0.05) -- Wait briefly between teleports
+                    end
+                end
+            end
+        end 
     end
 end);
 --AntiAdmin Function
 spawn(function()
     while wait(1) do
-        if not myself then
-            return
-        end
-    
-        for _, player in ipairs(Players:GetPlayers()) do
-            if isUserAdmin(player.UserId) then
-                game:Shutdown()
+        if AntiAdmin then
+            if not myself then
+                return
             end
+        
+            for _, player in ipairs(Players:GetPlayers()) do
+                if isUserAdmin(player.UserId) then
+                    game:Shutdown()
+                end
+            end 
         end
     end
 end);
